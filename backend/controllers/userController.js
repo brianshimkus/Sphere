310bfb3cf7dbdcad8d27e77cdff53c570c5a76be
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-07 00:47:44 -0500

    Generate JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
 			_id: user._id,
 			name: user.name,
 			email: user.email,
+			token: generateToken(user._id),
 		})
 	} else {
 		res.status(400)
@@ -59,6 +60,7 @@ const loginUser = asyncHandler(async (req, res) => {
 			_id: user._id,
 			name: user.name,
 			email: user.email,
+			token: generateToken(user._id),
 		})
 	} else {
 		res.status(401)
@@ -72,5 +74,12 @@ const loginUser = asyncHandler(async (req, res) => {
 const getUserData = asyncHandler(async (req, res) => {
 	res.json({ message: 'Get user data' })
 })
+
+// Generate token
+const generateToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: '30d',
+	})
+}
 
 export { registerUser, loginUser, getUserData }
