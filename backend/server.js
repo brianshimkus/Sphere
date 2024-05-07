ae991724d9859d9cb17179c2fe3b92e83c0800c7
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-06 22:38:58 -0500

    Initial job routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './db/connection.js'
+import jobsRoutes from './routes/jobsRoutes.js'
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/jobs', jobsRoutes)
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`)
