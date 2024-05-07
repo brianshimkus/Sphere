53daf6487edfdb9d9ca222605510bc84cc2f5565
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-06 19:34:58 -0500

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from './db/connection.js'
+
+dotenv.config()
 
 const PORT = process.env.PORT || 5050
+
+connectDB()
+
 const app = express()
 
 app.use(cors())
